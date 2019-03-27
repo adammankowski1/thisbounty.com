ba3ac392c1af2287a8eb8c64b56fced075a8e86e
Steve Hartken <steve.hartken@gmail.com>
2019-03-27 15:14:10 -0400

    #6 -- ThisBounty Storybook
    Storybook setup for ThisBounty component

---
+++ src/components/ThisBounty/table.stories.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Table } from "../ThisBounty/table-component.js";
+
+storiesOf("ThisBounty", module).add("with table", () => <Table />);
